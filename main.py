5565418df4aa90b62d3987d1f27a2c6ab8ffc282
SameepSingh <sameep@uoguelph.ca>
2025-07-16 17:20:14 -0400

    changes

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
+import random
+
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1 
+
+
+ROWS = 3
+COL = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+
+
+    columns = []
+    for _ in range(cols):
+         column = []
+         current_symbols = all_symbols[:]
+         for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+         columns.append(column)
+
+    return columns
+
+
+    def print_slot_machine(columns):
+        for row in range(len(column[0])):
+            for i, column in enumerate(columns):
+                if i != len(columns) - 1:
+                    print(column[row], "|")
+                else:
+                    print(column[row])
+                                
+
 
 
 
@@ -57,8 +103,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You dont havbe enough money to bet that much, your current balance is: ${balance}")
+
+        else:
+            break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
